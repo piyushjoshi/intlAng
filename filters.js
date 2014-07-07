253924daa07cb849fcf9a6e5f5f62ec7b5201263
piyushjoshi <piyush2731@gmail.com>
2014-07-07 13:52:11 +0530

    added stops display filter

--- filters.js
+++ filters.js
@@ -6,3 +6,10 @@ modules.intlApp.filter("currencyWithoutDecimal", function() {
 		return value.replace(/\.\d+/, "");
 	};
 });
+
+modules.intlApp.filter("stopsDisplay", function() {
+	return function(value) {
+		value = +value;
+		return value === 0 ? "Non stop" : value === 1 ? "1 stop" : value + " stops";
+	};
+});
